913ea83767c91c426d5ed8bd1098da64462f1c77
Carles <Carles@10.0.2.15>
2021-08-26 13:03:39 +0200

    prova git

--- M06_Nivell3_JobsProject/src/com/jobs/view/Main.java
+++ M06_Nivell3_JobsProject/src/com/jobs/view/Main.java
@@ -1,5 +1,6 @@
 package com.jobs.view;
 
+
 import com.jobs.application.JobsController;
 
 public class Main {
@@ -10,15 +11,15 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		
-		controller.createBossEmployee("Pepe Boss", "Dirección molona", "666666666", 6000.0);
-		controller.createEmployee("Pedro Employee", "Dirección molona 2", "665266666", 4000.0);
-		controller.createEmployee("Laura Employee", "Dirección molona 3", "625266666", 45.0);
-		controller.createVolunteer("Juan Volunteer", "Dirección molona 4", "614266666");
-		controller.createManagerEmployee("Pedro Employee", "Dirección molona 2", "665226666", 4500.0);
+		controller.createBossEmployee("Pepe Boss", "Direcciï¿½n molona", "666666666", 6000.0);
+		controller.createEmployee("Pedro Employee", "Direcciï¿½n molona 2", "665266666", 4000.0);
+		controller.createEmployee("Laura Employee", "Direcciï¿½n molona 3", "625266666", 45.0);
+		controller.createVolunteer("Juan Volunteer", "Direcciï¿½n molona 4", "614266666");
+		controller.createManagerEmployee("Pedro Employee", "Direcciï¿½n molona 2", "665226666", 4500.0);
 		//[CFV - 21/07/2021] -- Nous empleats Nivell 2
-		controller.createJuniorEmployee("Employee Junior", "Dirección molona 5", "625555555", 1200);
-		controller.createMidEmployee("Employee Mid", "Dirección molona 6", "626666666", 2000);
-		controller.createSeniorEmployee("Employee Senior", "Dirección molona 7", "627777777", 3000);
+		controller.createJuniorEmployee("Employee Junior", "Direcciï¿½n molona 5", "625555555", 1200);
+		controller.createMidEmployee("Employee Mid", "Direcciï¿½n molona 6", "626666666", 2000);
+		controller.createSeniorEmployee("Employee Senior", "Direcciï¿½n molona 7", "627777777", 3000);
 		
 		controller.payAllEmployeers();
 		
